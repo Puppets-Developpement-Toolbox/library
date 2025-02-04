0c2bdc283ce18909f195f41c3c1fe344fac6ba32
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-04 16:15:17 +0100

    + revert / grid -> flex

--- src/templates/blocs/text_image.php
+++ src/templates/blocs/text_image.php
@@ -1,17 +1,23 @@
 
 
 
-<section class="[ section ]">
-  <div class="laptop:grid laptop:grid-cols-12 laptop:gap-0">
+<?php
+  // $revert = true;// Inversion positions image/texte
+?>
 
-    <figure class="[ img__cover ]
-                  aspect-[var(--ratio-img-mobile)] mb-12
-                  laptop:col-span-6 laptop:mb-0">
-      <?= carlo_get('image') ?>
-    </figure>
+<section class="[ section ]">
+  <div class="laptop:flex <?php if ($revert): ?> flex-row-reverse <?php endif ?> laptop:justify-between">
+
+    <div class="mb-12
+                laptop:shrink-0 laptop:flex laptop:items-center laptop:w-1/2 laptop:mb-0">
+      <figure class="[ img__cover ]
+                    aspect-[var(--ratio-img-mobile)]">
+        <?= carlo_get('image') ?>
+      </figure>
+    </div>
     
-    <section class="laptop:col-span-5 laptop:col-end-13 laptop:flex laptop:items-center">
-      <div>
+    <section class="laptop:w-1/2 laptop:flex laptop:items-center">
+      <div class="laptop:w-5/6 laptop:max-w-5/6 <?php if (!$revert): ?> laptop:ml-auto <?php endif ?>">
         <header class="mb-6">
           <?= carlo_get('surtitle') ?>
           <h2 class="[ h2 ]">
