4e48c4c3ab02d45f9103881fea160f3c928293e8
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-04 17:17:42 +0100

    + square / bg-primary

--- src/templates/blocs/text_image.php
+++ src/templates/blocs/text_image.php
@@ -2,27 +2,39 @@
 
 
 <?php
-  // $revert = true;// Inversion positions image/texte
+
+  // $bgprimary = true;
+  
+  $padding = (isset($bgprimary) && $bgprimary) ? 'px-8 py-10 laptop:pl-0' : 'pt-12 laptop:p-0';
+  $ratio = (isset($bgprimary) && $bgprimary) ? 'aspect-square' : 'aspect-[var(--ratio-img-mobile)]';
+  $bg = (isset($bgprimary) && $bgprimary) ? 'bg-primary' : '';
+  $text = (isset($bgprimary) && $bgprimary) ? 'text-white' : '';
+
+  // Inversion distribution image/texte
+  // $revert = true;
+
 ?>
 
 <section class="[ section ]">
-  <div class="laptop:flex <?php if ($revert): ?> flex-row-reverse <?php endif ?> laptop:justify-between">
+  <div class="laptop:flex <?php if ($revert): ?> flex-row-reverse <?php endif ?> laptop:justify-between
+              <?= $bg ?>
+              group/section">
 
-    <div class="mb-12
-                laptop:shrink-0 laptop:flex laptop:items-center laptop:w-1/2 laptop:mb-0">
+    <div class="laptop:shrink-0 laptop:flex laptop:items-center laptop:w-1/2">
       <figure class="[ img__cover ]
-                    aspect-[var(--ratio-img-mobile)]">
+                    <?= $ratio ?>">
         <?= carlo_get('image') ?>
       </figure>
     </div>
     
-    <section class="laptop:w-1/2 laptop:flex laptop:items-center">
+    <section class="laptop:w-1/2 laptop:flex laptop:items-center
+                    <?= $text ?>
+                    <?= $padding ?>">
       <div class="laptop:w-5/6 laptop:max-w-5/6 <?php if (!$revert): ?> laptop:ml-auto <?php endif ?>">
         <header class="mb-6">
           <?= carlo_get('surtitle') ?>
-          <h2 class="[ h2 ]">
-            <?= carlo_get('title') ?>
-          </h2>
+          <h2 class="[ h2 ]
+                    <?= $text ?>"><?= carlo_get('title') ?></h2>
         </header>
         <?= carlo_get('description') ?>
         <?php if (carlo_get('cta_push')) carlo_render('components/cta_push', ['link' => '#', 'label' => 'En savoir plus']) ?>
--- src/templates/components/cta_push.php
+++ src/templates/components/cta_push.php
@@ -5,12 +5,13 @@
   <button type="button"
           class="group/btn
                 flex justify-center items-center w-auto px-6 py-3
-                group-[&]/hero:w-full
                 rounded-full bg-gradient-to-r from-white from-50% to-primary to-50% border-[1px] bg-right bg-[size:_200%] border-primary
                 text-white
                 transition-all duration-300
-                group-[&]/hero:laptop:w-auto
-                hover:bg-left hover:text-primary">
+                group-[.bg-primary]/section:border-white
+                group-[&]/hero:w-full group-[&]/hero:laptop:w-auto
+                hover:bg-left hover:text-primary
+                group-[&]/section:laptop:mt-8">
     <span class="shrink-0 size-6 text-accent
                 transition-transform duration-300
                 group-hover/btn:rotate-180">
