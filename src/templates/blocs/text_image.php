a74f3c76e6e7460be3bf5fc878b73db23dfe5af8
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-04 15:22:03 +0100

    + desktop

--- src/templates/blocs/text_image.php
+++ src/templates/blocs/text_image.php
@@ -2,17 +2,26 @@
 
 
 <section class="[ section ]">
-  <figure class="[ img__cover ]
-                aspect-[var(--ratio-img-mobile)] mb-12">
-    <?= carlo_get('image') ?>
-  </figure>
-  <header class="mb-6">
-    <?= carlo_get('surtitle') ?>
-    <h2 class="[ h2 ]">
-      <?= carlo_get('title') ?>
-    </h2>
-  </header>
-  <?= carlo_get('description') ?>
+  <div class="laptop:grid laptop:grid-cols-12 laptop:gap-0">
 
-  <?php if (carlo_get('cta_push')) carlo_render('components/cta_push', ['link' => '#', 'label' => 'En savoir plus']) ?>
+    <figure class="[ img__cover ]
+                  aspect-[var(--ratio-img-mobile)] mb-12
+                  laptop:col-span-6 laptop:mb-0">
+      <?= carlo_get('image') ?>
+    </figure>
+    
+    <section class="laptop:col-span-5 laptop:col-end-13 laptop:flex laptop:items-center">
+      <div>
+        <header class="mb-6">
+          <?= carlo_get('surtitle') ?>
+          <h2 class="[ h2 ]">
+            <?= carlo_get('title') ?>
+          </h2>
+        </header>
+        <?= carlo_get('description') ?>
+        <?php if (carlo_get('cta_push')) carlo_render('components/cta_push', ['link' => '#', 'label' => 'En savoir plus']) ?>
+      </div>
+    </section>
+  
+  </div>
 </section>
