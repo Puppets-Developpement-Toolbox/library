bc588f544eff5ac53e6365ea06ff8efd8d59e38b
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-07 11:14:05 +0100

    upd card

--- src/templates/sections/push/base.php
+++ src/templates/sections/push/base.php
@@ -36,25 +36,27 @@
             laptop:grid laptop:grid-cols-3">
     <?php foreach ($slides as $slide): ?>
     <li class="group/card">
-      <figure class="[ img__cover ]
-                    aspect-[8/5] overflow-clip">
-        <div class="scale-115
-                    transition-transform duration-500
-                    group-hover/card:scale-100">
-          <?= $slide['image'] ?>
+      <div class="flex flex-col h-full">
+        <figure class="[ img__cover ]
+                      shrink-0 aspect-[8/5] overflow-clip">
+          <div class="scale-115
+                      transition-transform duration-500
+                      group-hover/card:scale-100">
+            <?= $slide['image'] ?>
+          </div>
+        </figure>
+        <div class="flex-1 flex flex-col gap-6 p-8
+                    border-1 border-t-0 border-black">
+          <header class="flex flex-col gap-2
+                        after:transition-transform after:duration-500
+                        after:block after:origin-left after:w-full after:h-[1px] after:mt-4 after:bg-black
+                        group-hover/card:after:scale-x-85">
+            <h3 class="[ h3 ]"><?= $slide['title'] ?></h3>
+            <span class="[ kicker-subtitle ]"><?= $slide['subtitle'] ?></span>
+          </header>
+          <div class="flex-1"><?= $slide['content'] ?></div>
+          <?php carlo_render('components/cta:tertiary', ['link' => $slide['cta']['link'], 'label' => $slide['cta']['label']]) ?>
         </div>
-      </figure>
-      <div class="flex flex-col gap-6 p-8
-                  border-1 border-t-0 border-black">
-        <header class="flex flex-col gap-2
-                      after:transition-transform after:duration-500
-                      after:block after:origin-left after:w-full after:h-[1px] after:mt-4 after:bg-black
-                      group-hover/card:after:scale-x-85">
-          <h3 class="[ h3 ]"><?= $slide['title'] ?></h3>
-          <span class="[ kicker-subtitle ]"><?= $slide['subtitle'] ?></span>
-        </header>
-        <div><?= $slide['content'] ?></div>
-        <?php carlo_render('components/cta:tertiary', ['link' => $slide['cta']['link'], 'label' => $slide['cta']['label']]) ?>
       </div>
     </li>
     <?php endforeach ?>
