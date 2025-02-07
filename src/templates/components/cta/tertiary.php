dd09ce59b1208b6030bec3360072139fe50e1ebc
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-07 11:34:55 +0100

    + tertiary CTA

--- src/templates/components/cta/tertiary.php
+++ src/templates/components/cta/tertiary.php
@@ -4,12 +4,14 @@
 <a href="<?= carlo_get('link') ?>">
   <button type="button"
           class="group/btn
-                flex justify-center items-center gap-1 w-auto px-6 py-3
-                group-[&]/hero:w-full
-                rounded-full bg-gradient-to-r from-primary from-50% to-white to-50% border-[1px] bg-right bg-[size:_200%] border-primary
-                transition-all duration-300
-                group-[&]/hero:laptop:w-auto
-                hover:bg-left hover:text-white">
+                flex justify-center items-center gap-1 w-auto py-3
+                text-primary font-bold
+                hover:text-black">
+    <span class="shrink-0 size-6 text-accent
+                transition-transform duration-300
+                group-hover/btn:rotate-180">
+      <svg viewBox="0 0 24 24"><use xlink:href="#svg__more"></use></svg>
+    </span>
     <span><?= carlo_get('label') ?></span>
   </button>
 </a>
