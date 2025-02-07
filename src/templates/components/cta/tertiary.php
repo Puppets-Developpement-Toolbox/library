287865f5377586981a96f7f54b3a61ec4fbf24ab
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-07 10:17:47 +0100

    + hover

---
+++ src/templates/components/cta/tertiary.php
@@ -0,0 +1,15 @@
+
+
+
+<a href="<?= carlo_get('link') ?>">
+  <button type="button"
+          class="group/btn
+                flex justify-center items-center gap-1 w-auto px-6 py-3
+                group-[&]/hero:w-full
+                rounded-full bg-gradient-to-r from-primary from-50% to-white to-50% border-[1px] bg-right bg-[size:_200%] border-primary
+                transition-all duration-300
+                group-[&]/hero:laptop:w-auto
+                hover:bg-left hover:text-white">
+    <span><?= carlo_get('label') ?></span>
+  </button>
+</a>
--- src/templates/sections/push/base.php
+++ src/templates/sections/push/base.php
@@ -30,18 +30,26 @@
   <?php if ($slides): ?>
   <ul class="flex flex-col gap-12">
     <?php foreach ($slides as $slide): ?>
-    <li>
-      <figure class="[ img__cover ] aspect-[8/5]">
-        <?= $slide['image'] ?>
+    <li class="group/card">
+      <figure class="[ img__cover ]
+                    aspect-[8/5] overflow-clip">
+        <div class="scale-[115%]
+                    transition-transform duration-500
+                    group-hover/card:scale-100">
+          <?= $slide['image'] ?>
+        </div>
       </figure>
-      <div class="p-8
+      <div class="flex flex-col gap-6 p-8
                   border-1 border-t-0 border-black">
-        <header class="flex flex-col gap-1 mb-6 pb-4
-                      border-b-1 border-b-black">
+        <header class="flex flex-col gap-2
+                      after:transition-transform after:duration-500
+                      after:block after:origin-left after:w-full after:h-[1px] after:mt-4 after:bg-black
+                      group-hover/card:after:scale-x-90">
           <h3 class="[ h3 ]"><?= $slide['title'] ?></h3>
           <span class="[ kicker-subtitle ]"><?= $slide['subtitle'] ?></span>
         </header>
-        <?= $slide['content'] ?>
+        <div><?= $slide['content'] ?></div>
+        <?php carlo_render('components/cta:tertiary', ['link' => $slide['cta']['link'], 'label' => $slide['cta']['label']]) ?>
       </div>
     </li>
     <?php endforeach ?>
