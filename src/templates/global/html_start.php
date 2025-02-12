78dd6a017bbea68e2f6cda8ae23fdcb1fa5462b5
Samuel Laulhau <samuel.laulhau@puppets.fr>
2025-02-12 10:34:11 +0100

    clean

--- src/templates/global/html_start.php
+++ src/templates/global/html_start.php
@@ -10,28 +10,12 @@
 		<meta charset="utf-8">
 		<title>XXX_title_XXX</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
-		<style>
-      :root {
-        --accent: #F9B943;
-        --primary: #232660;
-        --primary-light: #45C0EB;
-        --secondary: #585A86;
-        --secondary-light: #8C8EAB;
-        --secondary-dark: #393C70;
 
-        --black: #121212;
-        --white: #FFFFFF;
 
-        --gray-light: #F6F6F6;
-        --gray-dark: #9C9C9C;
-      }
-  	</style>
-
-		<link href="https://front.library.lndo.site/src/css/global.css" rel="stylesheet">
+		<link href="https://front.library.lndo.site/library.css" rel="stylesheet">
 		<script type="module" src="https://front.library.lndo.site/@vite/client"></script>
     <script type="module" src="https://front.library.lndo.site/src/js/main.js"></script>
-		
+
 		<meta name="description" content="XXX_description_XXX">
 		<meta property="og:title" content="XXX_title_XXX">
 		<meta property="og:description" content="XXX_description_XXX">
@@ -41,17 +25,17 @@
     <meta property="og:locale" content="fr_FR">
 		<meta property="fb:app_id" content="XXX_fb_id_XXX">
 		<meta property="og:url" content="XXX_url_XXX">
-		
+
 		<meta name="twitter:card" content="summary_large_image">
 		<meta name="twitter:site" content="XXX_@creator_XXX">
     <meta name="twitter:dnt" content="on">
-		
+
     <meta name="format-detection" content="telephone=no">
-    
+
 		<link rel="canonical" href="XXX_url_XXX">
 	</head>
-  
-	
-	
+
+
+
 	<body>
-		<?php carlo_render('global/svg') ?>
+		<?php carlo_render("global/svg"); ?>
