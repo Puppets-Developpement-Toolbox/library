419a347c354d14d2f6c3e5c89581634fb922d91c
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-14 18:06:36 +0100

    + section text

---
+++ src/templates/sections/text/base.php
@@ -0,0 +1,9 @@
+
+
+
+<section class="[ section ]
+                laptop:grid laptop:grid-cols-12">
+  <div class="laptop:col-span-10 laptop:col-start-2">
+    <?= carlo_get('content') ?>
+  </div>
+</section>
