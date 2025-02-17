136c4a75f75de83bd2161e68df4e1b10d922a359
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-17 09:57:27 +0100

    +banner text

---
+++ src/templates/sections/hero_banner/text.php
@@ -0,0 +1,19 @@
+
+
+
+<section class="[ section ]
+                group/on-primary
+                mx-0 px-6 py-16 max-w-none
+                bg-primary
+                text-white
+                laptop:py-16.25">
+  <div class="flex flex-col gap-6 pl-6 max-w-150 mx-auto
+              border-l-1 border-l-white">
+    <h1 class="[ h1 ] text-white">
+      <?= carlo_get('title') ?>
+    </h1>
+    <div class="[ large ] text-white">
+      <?= carlo_get('subtitle') ?>
+    </div>
+  </div>
+</section>
