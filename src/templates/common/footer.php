e561e58538302ffc90d9630a895e310c60e86300
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-17 11:44:18 +0100

    + foote rmobile

---
+++ src/templates/common/footer.php
@@ -0,0 +1,96 @@
+
+
+
+<?php // dump(carlo_get()) ?>
+
+<footer class="group/on-primary
+              flex flex-col gap-8 mt-17.5 py-8 px-6
+              bg-primary
+              text-white">
+
+  <section class="[ contact ]
+                  py-8 px-10
+                  bg-secondary-dark">
+    
+    <span class="block mb-4 pb-4
+                border-b-1 border-b-secondary-light
+                font-semibold uppercase">Nous contacter</span>
+    
+    <div class="flex gap-4">
+      <span class="shrink-0 size-6">
+        <svg viewBox="0 0 20 20"><use xlink:href="#svg__social-linkedin"></use></svg>
+      </span>
+
+      <ul class="[ fade-not-hovered ]
+                flex flex-col gap-2 mb-4">
+        <?php //= carlo_get('linkedin'); // TODO: Doesn't work ?>
+        <li class="block relative ml-4
+                  before:block before:absolute before:top-3 before:-left-4 before:mr-2 before:size-1 before:rounded-full before:bg-accent">
+          <a href="https://www.linkedin.com/in/tiphaine-vapaille-6a39281/"
+            target="_blank">Tiphaine Vapaille</a>
+        </li>
+        <li class="block relative ml-4
+                  before:block before:absolute before:top-3 before:-left-4 before:mr-2 before:size-1 before:rounded-full before:bg-accent">
+          <a href="https://www.linkedin.com/in/sophie-beignon-02a60b1/"
+            target="_blank">Sophie Beignon</a>
+        </li>
+        <li class="mt-2">
+          <a href="mailto:diapsodie@gmail.com"
+            class="font-semibold underline underline-offset-3">diapsodie@gmail.com</a>
+        </li>
+      </ul>
+    </div>
+
+  </section>
+
+
+
+
+
+  <section class="[ main-links ]
+                  flex flex-col gap-4">
+    <header class="font-semibold uppercase text-secondary-dark">Liens principaux</header>
+    <ul class="[ fade-not-hovered ]
+                flex flex-col gap-4">
+      <!-- TODO: carlo_get() for menu -->
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Nos outils</a></li>
+      <li><a href="#">Nos offres</a></li>
+      <li><a href="#">Nos réalisations</a></li>
+      <li><a href="#">Qui sommes-nous ?</a></li>
+    </ul>
+  </section>
+
+
+
+
+
+<section class="[ useful-links ]
+                flex flex-col gap-4">
+  <header class="font-semibold uppercase text-secondary-dark">Liens utiles</header>
+  <ul class="[ fade-not-hovered ]
+              flex flex-col gap-4">
+    <!-- TODO: carlo_get() for menu -->
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Blog</a></li>
+  </ul>
+</section>
+
+
+
+
+
+<section class="[ legal-links ]
+                flex flex-col gap-4">
+  <header class="font-semibold uppercase text-secondary-dark">Liens principaux</header>
+  <ul class="[ fade-not-hovered ]
+              flex flex-col gap-4">
+    <!-- TODO: carlo_get() for menu -->
+    <li><a href="#">Mentions légales</a></li>
+    <li><a href="#">Confidentialité</a></li>
+    <li><a href="#">Politique de gestion des cookies</a></li>
+    <li><a href="#">CGV - CGU</a></li>
+  </ul>
+</section>
+
+</footer>
