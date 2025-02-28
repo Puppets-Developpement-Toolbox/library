510aa6d5a42f7d32d14f5fdd20481f612683b6ba
arnaud <arnaud.mercier@puppets.fr>
2025-02-28 16:28:35 +0100

    section image base carlo_get en carlo_img

--- src/templates/sections/image/base.php
+++ src/templates/sections/image/base.php
@@ -5,6 +5,6 @@
                 laptop:grid laptop:grid-cols-12">
   <figure class="[ img__cover ]
                 laptop:col-span-10 laptop:col-start-2">
-    <?= carlo_get('image') ?>
+    <?= carlo_img('image', '435x363') ?>
   </figure>
 </section>
