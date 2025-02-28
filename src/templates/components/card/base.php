c3c80fa198e2c276acba1a4849abc25190926ad0
PixelPoetryFR <julien@pixelpoetry.fr>
2025-02-28 10:58:55 +0100

    fix figure margin / upd padding text on laptop

--- src/templates/components/card/base.php
+++ src/templates/components/card/base.php
@@ -4,7 +4,7 @@
             group/card
             flex flex-col h-full">
   <figure class="[ img__cover ]
-                shrink-0 aspect-[8/5] overflow-clip">
+                shrink-0 aspect-[8/5] overflow-clip m-0">
     <div class="scale-115 h-full
                 transition-transform duration-500
                 group-hover/card:scale-100">
@@ -13,7 +13,8 @@
   </figure>
   <div class="flex-1 flex flex-col gap-6 p-8
               border-1 border-t-0 border-black
-              group-[&]/on-primary:bg-white group-[&]/on-primary:border-none">
+              group-[&]/on-primary:bg-white group-[&]/on-primary:border-none
+              laptop:px-12">
     <header class="flex flex-col gap-2
                   after:transition-transform after:duration-500
                   after:block after:origin-left after:w-full after:h-[1px] after:mt-4 after:bg-black
