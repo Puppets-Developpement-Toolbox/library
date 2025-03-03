78bc65346e962a6589dbf077cecb9c75a3f94d1c
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-03 17:07:19 +0100

    upd CTA

--- src/templates/components/cta/base.php
+++ src/templates/components/cta/base.php
@@ -4,14 +4,14 @@
 <?php if(carlo_get('link') || carlo_get('label')): ?>
   <button href="<?= carlo_get('link') ?>" type="button"
           class="group/btn
-                flex justify-center items-center gap-1.5 w-auto px-6 py-3
+                flex justify-center items-center gap-1.5 w-auto px-6 py-3 w-fit
                 rounded-full bg-gradient-to-r from-white from-50% to-primary to-50% bg-right bg-[size:202%] bg-no-repeat border-[1px] border-primary
                 text-white
                 transition-all duration-300
                 hover:bg-left hover:text-primary
                 group-[.bg-primary]/section:border-white
                 group-[&]/on-primary:bg-gradient-to-l group-[&]/on-primary:border-white group-[&]/on-primary:text-primary group-[&]/on-primary:hover:text-white
-                group-[&]/hero:w-full group-[&]/hero:laptop:w-auto">
+                group-[&]/hero:w-full group-[&]/hero:laptop:w-fit">
     <span class="shrink-0 size-6 text-accent
                 transition-transform duration-300
                 group-hover/btn:rotate-180">
