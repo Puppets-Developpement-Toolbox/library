592b938489a76de478315dc51e6f7e76885493d0
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-04 17:52:57 +0100

    bloc text insight base : un encadré sur deux est sur fond blanc, peut-on commencer par un encadré blanc comme sur la maquette ?

--- src/templates/components/insight/base.php
+++ src/templates/components/insight/base.php
@@ -3,13 +3,13 @@
 <?php if(carlo_get('details') || carlo_get('content')): ?>
 <div class="flex flex-col gap-6 p-8
             border-1 border-white
-            group-even/insight:bg-white group-even/insight:text-primary">
+            group-odd/insight:bg-white group-odd/insight:text-primary">
 <?php if(!empty(carlo_get('title')) || !empty(carlo_get('details'))): ?>
   <header class="[ h5 ]
                 flex flex-col gap-2 pb-4
                 border-b-1 border-b-white
                 text-white uppercase
-                group-even/insight:border-primary group-even/insight:text-primary
+                group-odd/insight:border-primary group-odd/insight:text-primary
                 laptop:flex-row laptop:justify-between laptop:items-center laptop:gap-4">
     <span class="mb-0 laptop:flex-1 laptop:max-w-[70%]"><?= carlo_get('title') ?></span>
     <small class="text-primary-light"><?= carlo_get('details') ?></small>
