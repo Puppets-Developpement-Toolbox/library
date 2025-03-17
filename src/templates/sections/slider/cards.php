f9700558369c5585b0240ca842d652466f4f0cf6
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-17 12:21:54 +0100

    rebuild header flex -> grid

--- src/templates/sections/slider/cards.php
+++ src/templates/sections/slider/cards.php
@@ -16,54 +16,51 @@
     <section class="[ section has-slider slider-cards ]
                     flex flex-col gap-8
                     group-[&]/on-primary:py-16 group-[&]/on-primary:bg-transparent group-[&]/on-primary:laptop:py-30">
-    <header>
+    <header class="laptop:grid laptop:grid-cols-12 laptop:gap-6">
         <h2 class="[ h2 ]
-                  mb-6
                   group-[&]/on-primary:text-white
-                  laptop:col-span-12">
-        <?=str_replace(' >', '</em>', str_replace('< ', '<em>', carlo_get("title"))); ?>
+                  laptop:col-span-8">
+        <?= str_replace(' >', '</em>', str_replace('< ', '<em>', carlo_get("title"))); ?>
         </h2>
 
-        <div class="laptop:grid laptop:grid-cols-12">
-          <div class="laptop:col-span-8">
-            <?= carlo_get("description") ?>
-          </div>
-          <aside class="hidden
-                        laptop:col-span-4 laptop:flex laptop:justify-end laptop:gap-4">
-            <div class="[ swiper__dashboard ]
-                        flex gap-2">
-              <button type="button"
-                      class="[ swiper-button-prev ]
-                            group/btn
-                            inline-block !static !left-6 !size-12 !m-0
-                            rounded-full border-1 border-primary bg-transparent
-                            !text-primary
-                            group-[&]/on-primary:!text-white group-[&]/on-primary:border-white
-                            after:!content-none">
-                <svg viewBox="0 0 24 24"
-                      class="!size-6
-                            transition-transform duration-300
-                            group-hover/btn:-translate-x-1/2"><use href="#svg__arrow"></use></svg>
-              </button>
-              <button type="button"
-                      class="[ swiper-button-next ]
-                            group/btn
-                            inline-block !static !right-6 !size-12 !m-0
-                            rounded-full border-1 border-primary bg-transparent
-                            !text-primary
-                            group-[&]/on-primary:!text-white group-[&]/on-primary:border-white
-                            after:!content-none">
-                <svg viewBox="0 0 24 24"
-                      class="!size-6 rotate-180
-                            transition-transform duration-300
-                            group-hover/btn:translate-x-1/2"><use href="#svg__arrow"></use></svg>
-              </button>
-            </div>
-            <?php if (carlo_get("cta")) {
-                carlo_render("components/cta", carlo_get('cta'));
-            } ?>
-          </aside>
+        <div class="laptop:col-span-8">
+          <?= carlo_get("description") ?>
         </div>
+        <aside class="hidden
+                      laptop:col-span-4 laptop:self-end laptop:flex laptop:justify-end laptop:items-center laptop:gap-4">
+          <div class="[ swiper__dashboard ]
+                      flex gap-2">
+            <button type="button"
+                    class="[ swiper-button-prev ]
+                          group/btn
+                          shrink-0 inline-block !static !left-6 !size-12 !m-0
+                          rounded-full border-1 border-primary bg-transparent
+                          !text-primary
+                          group-[&]/on-primary:!text-white group-[&]/on-primary:border-white
+                          after:!content-none">
+              <svg viewBox="0 0 24 24"
+                    class="!size-6
+                          transition-transform duration-300
+                          group-hover/btn:-translate-x-1/2"><use href="#svg__arrow"></use></svg>
+            </button>
+            <button type="button"
+                    class="[ swiper-button-next ]
+                          group/btn
+                          shrink-0 inline-block !static !right-6 !size-12 !m-0
+                          rounded-full border-1 border-primary bg-transparent
+                          !text-primary
+                          group-[&]/on-primary:!text-white group-[&]/on-primary:border-white
+                          after:!content-none">
+              <svg viewBox="0 0 24 24"
+                    class="!size-6 rotate-180
+                          transition-transform duration-300
+                          group-hover/btn:translate-x-1/2"><use href="#svg__arrow"></use></svg>
+            </button>
+          </div>
+          <?php if (carlo_get("cta")) {
+              carlo_render("components/cta", carlo_get('cta'));
+          } ?>
+        </aside>
       </header>
 
 
