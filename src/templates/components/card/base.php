4566fd0eb3668be24517d884c1d340935d9aee3c
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-17 15:52:43 +0100

    - link

--- src/templates/components/card/base.php
+++ src/templates/components/card/base.php
@@ -4,10 +4,7 @@
             group/card
             flex flex-col h-full
             bg-white">
-<?php $link = carlo_get('link'); if(!empty($link)): ?>
-    <a href="<?=$link?>">
-<?php endif; ?>
-<?php if(!empty(carlo_get('image'))): ?>
+  <?php if(!empty(carlo_get('image'))): ?>
     <figure class="[ img__cover ]
                 shrink-0 aspect-[8/5] overflow-clip !m-0">
     <div class="aspect-[8/5] scale-115 h-full
@@ -16,7 +13,7 @@
       <?= carlo_img('image', '630x394') ?>
     </div>
   </figure>
-<?php endif; ?>
+  <?php endif; ?>
   <div class="flex-1 flex flex-col gap-6 mb-[1px] p-8
               border-1 <?=!carlo_get('border')? 'border-t-0' : '' ?> border-black
               group-[&]/on-primary:bg-white group-[&]/on-primary:border-none
@@ -26,7 +23,7 @@
                   after:block after:origin-left after:w-full after:h-[1px] after:mt-4 after:bg-black
                   group-hover/card:after:scale-x-85">
       <h3 class="[ h3 ]"><?= str_replace(' >', '</em>', str_replace('< ', '<em>', carlo_get("title"))); ?></h3>
-      <span class="[ kicker-subtitle ]"><?= carlo_get('subtitle') ?></span>
+      <?php if (carlo_get('subtitle')): ?><span class="[ kicker-subtitle ]"><?= carlo_get('subtitle') ?></span><?php endif ?>
     </header>
     <div class="[ text ]
                 flex-1
@@ -38,7 +35,4 @@
     <?php carlo_render('components/cta:tertiary', carlo_get('cta')) ?>
     <?php endif; ?>
   </div>
-<?php if(!empty($link)): ?>
-</a>
-<?php endif; ?>
 </div>
