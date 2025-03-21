43fc734b0581f0693b4ffc93a41da30f0f855e8b
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-21 13:10:27 +0100

    Home > Nos offres > le CTA blanc devrait devenir bleu au hover

--- src/templates/components/cta/secondary.php
+++ src/templates/components/cta/secondary.php
@@ -7,10 +7,11 @@
                 flex justify-center items-center gap-1 w-auto px-6 py-3
                 group-[&]/hero:w-full
                 rounded-full bg-gradient-to-r from-primary from-50% to-white to-50% border-[1px] bg-right bg-[size:_200%] border-primary
+                text-primary
                 transition-all duration-300
                 group-[&]/hero:laptop:w-auto
-                text-primary
-                <?php if(!empty(carlo_get('cta_hover'))) : ?>hover:bg-left hover:text-white<?php endif; ?>">
+                group-[&]/on-primary:border-white
+                hover:bg-left hover:text-white">
     <span class="-mt-0.5"><?= carlo_get('label') ?></span>
   </button>
 </a>
