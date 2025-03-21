eed3a2ad3a8d79356c7cc0e9ca0bd356dc2707d5
PixelPoetryFR <julien@pixelpoetry.fr>
2025-03-21 17:17:47 +0100

    line-clamp-5 Home - nos projets > la hauteur des rectangles par projet est trop grande vs maquette

--- src/templates/components/card/base.php
+++ src/templates/components/card/base.php
@@ -31,7 +31,7 @@
                 flex-1
                 group-[&]/on-primary:text-primary">
         <div class="flex flex-col gap-6
-                    group-[&]/cards-limit-lines:line-clamp-4 group-[&]/cards-limit-lines:overflow-y-clip group-[&]/cards-limit-lines:max-h-24">
+                    group-[&]/cards-limit-lines:line-clamp-5 group-[&]/cards-limit-lines:overflow-y-clip group-[&]/cards-limit-lines:max-h-32">
           <?= carlo_get('content') ?>
         </div>
     </div>
